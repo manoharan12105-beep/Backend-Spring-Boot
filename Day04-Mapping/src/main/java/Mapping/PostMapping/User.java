7b34b5633f738d97308575906f41ce4253305a0e
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-15 11:53:18 +0530

    Create User class

---
+++ Day04-Mapping/src/main/java/Mapping/PostMapping/User.java
@@ -0,0 +1,11 @@
+package com.example.SpringApplication.Mapping;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class User {
+  private String Name;
+  private int Age;
+}
