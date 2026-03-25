d3e90c8bbaca05ffde21d3d1eca59fed24eee9d5
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-25 11:06:07 +0530

    Create StudentRepository.java

---
+++ Day05-H2-Database/src/main/StudentRepository.java
@@ -0,0 +1,8 @@
+package H2.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
